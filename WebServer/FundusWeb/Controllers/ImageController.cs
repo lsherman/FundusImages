4081741e7256806ee19ee0561ad60794c4ccf20b
lsherman <LucasASherman@gmail.com>
2013-11-21 12:54:09 -0600

    - Integrated OpenCL segmentation library, removed MATLAB dependencies

--- WebServer/FundusWeb/Controllers/ImageController.cs
+++ WebServer/FundusWeb/Controllers/ImageController.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Text.RegularExpressions;
 using System.Diagnostics;
 using System.Web;
+using System.Runtime.InteropServices;
 
 namespace FundusWeb.Controllers
 {
@@ -28,58 +29,20 @@ namespace FundusWeb.Controllers
             var data     = match.Groups["data"].Value;
 
             // **
-            // Store the image file locally for segmentation operations
+            // Call the segmentation service to segment the image
             // **
             var appdir = HttpContext.Current.Server.MapPath("~");
             var ibytes = Convert.FromBase64String(data);
-            var ifile = appdir + "filename." + mime.Replace("image/", "");
-            try
-            {
-                File.WriteAllBytes(ifile, ibytes);
-            }
-            catch (DirectoryNotFoundException e)
-            {
-                var i = 0;
-            }
-            catch (UnauthorizedAccessException e)
-            {
-                var i = 0;
-            }
 
-            // **
-            // Execute the segmentation process on the image file
-            // **
-            // :NOTE: For debug, set Window=true, Shell=true, WindowStyle=Normal
-            //        else Window=false, Shell=false, WindowStyle=Hidden
-            // :NOTE: In any case this is temp until we do a direct call to DLL
-            //        from within the server thread.
-            ProcessStartInfo startInfo = new ProcessStartInfo();
-            startInfo.CreateNoWindow = true;
-            startInfo.UseShellExecute = true;
-            startInfo.WorkingDirectory = appdir;
-            startInfo.FileName = appdir + "Vasc_detection_rework_DRIVE_classify_DRIVE_trn.exe";
-            startInfo.WindowStyle = ProcessWindowStyle.Normal;
-            startInfo.Arguments = ifile;
+            IntPtr outStr = (IntPtr)0;
+            IntPtr outLen = (IntPtr)0;
+            IntPtr length = (IntPtr)ibytes.Length;
+            FundusSegment.segmentImage(ibytes, length, ref outStr, ref outLen);
+            byte[] obytes = new byte[(int)outLen];
+            Marshal.Copy((IntPtr)outStr, obytes, 0, (int)outLen);
 
-            try
-            {
-                using (Process exeProcess = Process.Start(startInfo)) { exeProcess.WaitForExit(); }
-            }
-            catch
-            {
-                // Log error.
-
-                return new FundusImage { id = image.id, data = "" }; 
-            }
-
-            // **
-            // Load the output file from the segmentation 
-            // **
-            var ofile      = appdir + "Data\\DRIVE segmented\\01_test.tif";
-            var obytes     = File.ReadAllBytes(ofile);
             var ob64String = Convert.ToBase64String(obytes);
-            var dataUrl    = "data:image/tiff"
-                //+ Path.GetExtension(ofile).Replace(".", "")
+            var dataUrl    = "data:image/jpeg"
                 + ";base64," 
                 + ob64String;
 
--- WebServer/FundusWeb/Global.asax.cs
+++ WebServer/FundusWeb/Global.asax.cs
@@ -9,9 +9,6 @@ using System.Web.Routing;
 
 namespace FundusWeb
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
-
     public class WebApiApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
@@ -22,6 +19,9 @@ namespace FundusWeb
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var appdir = HttpContext.Current.Server.MapPath("~");
+            FundusWeb.Models.FundusSegment.startup(appdir);
         }
     }
 }
